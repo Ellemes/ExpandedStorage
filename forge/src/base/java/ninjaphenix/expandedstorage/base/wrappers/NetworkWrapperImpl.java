80b2a30230c5aaa34cf0e991420a2a7a583428bb
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-08-23 22:44:14 +0100

    Sync server options, WIP

--- forge/src/base/java/ninjaphenix/expandedstorage/base/wrappers/NetworkWrapperImpl.java
+++ forge/src/base/java/ninjaphenix/expandedstorage/base/wrappers/NetworkWrapperImpl.java
@@ -5,27 +5,30 @@ import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fmllegacy.network.NetworkDirection;
 import net.minecraftforge.fmllegacy.network.NetworkRegistry;
+import net.minecraftforge.fmllegacy.network.PacketDistributor;
 import net.minecraftforge.fmllegacy.network.simple.SimpleChannel;
 import ninjaphenix.expandedstorage.base.internal_api.Utils;
 import ninjaphenix.expandedstorage.base.internal_api.inventory.ServerMenuFactory;
 import ninjaphenix.expandedstorage.base.inventory.PagedMenu;
 import ninjaphenix.expandedstorage.base.inventory.ScrollableMenu;
 import ninjaphenix.expandedstorage.base.inventory.SingleMenu;
+import ninjaphenix.expandedstorage.base.network.NotifyServerOptionsMessage;
 import ninjaphenix.expandedstorage.base.network.ScreenTypeUpdateMessage;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
-final class NetworkWrapperImpl implements NetworkWrapper {
+public final class NetworkWrapperImpl implements NetworkWrapper {
     private static NetworkWrapperImpl INSTANCE;
     private final Map<UUID, ResourceLocation> playerPreferences = new HashMap<>();
     private final Map<ResourceLocation, ServerMenuFactory> menuFactories = Utils.unmodifiableMap(map -> {
@@ -34,6 +37,7 @@ final class NetworkWrapperImpl implements NetworkWrapper {
         map.put(Utils.PAGED_SCREEN_TYPE, PagedMenu::new);
     });
     private SimpleChannel channel;
+    private Set<ResourceLocation> screenOptions;
 
     public static NetworkWrapperImpl getInstance() {
         if (INSTANCE == null) {
@@ -43,15 +47,25 @@ final class NetworkWrapperImpl implements NetworkWrapper {
     }
 
     @SubscribeEvent
-    public static void onPlayerConnected(ClientPlayerNetworkEvent.LoggedInEvent event) {
+    public static void cOnPlayerConnected(ClientPlayerNetworkEvent.LoggedInEvent event) {
         NetworkWrapper.getInstance().c2s_sendTypePreference(ConfigWrapper.getInstance().getPreferredScreenType());
     }
 
     @SubscribeEvent
-    public static void onPlayerDisconnected(PlayerEvent.PlayerLoggedOutEvent event) {
+    public static void sOnPlayerConnected(PlayerEvent.PlayerLoggedInEvent event) {
+        NetworkWrapperImpl.INSTANCE.channel.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) event.getPlayer()), new NotifyServerOptionsMessage(NetworkWrapperImpl.INSTANCE.menuFactories.keySet()));
+    }
+
+    @SubscribeEvent
+    public static void sOnPlayerDisconnected(PlayerEvent.PlayerLoggedOutEvent event) {
         if (event.getPlayer() instanceof ServerPlayer player) { // Probably called on both sides.
             NetworkWrapper.getInstance().s_setPlayerScreenType(player, Utils.UNSET_SCREEN_TYPE);
         }
+    }
+
+    @SubscribeEvent
+    public static void cOnPlayerDisconnected(ClientPlayerNetworkEvent.LoggedOutEvent event) {
+
     }
 
     public void initialise() {
@@ -59,18 +73,14 @@ final class NetworkWrapperImpl implements NetworkWrapper {
         channel = NetworkRegistry.newSimpleChannel(Utils.resloc("channel"), () -> channelVersion, channelVersion::equals, channelVersion::equals);
 
         channel.registerMessage(0, ScreenTypeUpdateMessage.class, ScreenTypeUpdateMessage::encode, ScreenTypeUpdateMessage::decode, ScreenTypeUpdateMessage::handle, Optional.of(NetworkDirection.PLAY_TO_SERVER));
+        channel.registerMessage(1, NotifyServerOptionsMessage.class, NotifyServerOptionsMessage::encode, NotifyServerOptionsMessage::decode, NotifyServerOptionsMessage::handle, Optional.of(NetworkDirection.LOGIN_TO_CLIENT));
 
         if (PlatformUtils.getInstance().isClient()) {
-            MinecraftForge.EVENT_BUS.addListener(NetworkWrapperImpl::onPlayerConnected);
+            MinecraftForge.EVENT_BUS.addListener(NetworkWrapperImpl::cOnPlayerConnected);
+            MinecraftForge.EVENT_BUS.addListener(NetworkWrapperImpl::cOnPlayerDisconnected);
         }
-        MinecraftForge.EVENT_BUS.addListener(NetworkWrapperImpl::onPlayerDisconnected);
-    }
-
-    @Override
-    public void c2s_setSendTypePreference(ResourceLocation selection) {
-        if (ConfigWrapper.getInstance().setPreferredScreenType(selection)) {
-            this.c2s_sendTypePreference(selection);
-        }
+        MinecraftForge.EVENT_BUS.addListener(NetworkWrapperImpl::sOnPlayerConnected);
+        MinecraftForge.EVENT_BUS.addListener(NetworkWrapperImpl::sOnPlayerDisconnected);
     }
 
     @Override
@@ -97,7 +107,27 @@ final class NetworkWrapperImpl implements NetworkWrapper {
     }
 
     @Override
-    public void openInventoryAt(Level level, BlockPos pos) {
+    public Set<ResourceLocation> getScreenOptions() {
+        return screenOptions;
+    }
 
+    @Override
+    public void c_openInventoryAt(BlockPos pos) {
+
+    }
+
+    @Override
+    public void c_openInventoryAt(BlockPos pos, ResourceLocation selection) {
+
+    }
+
+    public void c_setServerOptions(Set<ResourceLocation> options) {
+        var newOptions = new HashSet<ResourceLocation>();
+        for (ResourceLocation option : options) {
+            if (this.isValidScreenType(option)) {
+                newOptions.add(option);
+            }
+        }
+        this.screenOptions = newOptions;
     }
 }
