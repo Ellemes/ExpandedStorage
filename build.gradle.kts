ce5ab0acc3c5d94fd6b44a79536a57abe4ea6e33
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2022-04-08 10:33:33 +0100

    quilt - depend on specific modules only.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("dev.architectury.loom").version("0.11.0-SNAPSHOT").apply(false)
     id("org.quiltmc.loom").version("0.12-SNAPSHOT").apply(false)
     id("com.github.johnrengelman.shadow").version("7.1.2").apply(false)
-    id("ninjaphenix.gradle.mod").version("6.2.1.9")
+    id("ninjaphenix.gradle.mod").version("6.2.1.10")
     //id("com.modrinth.minotaur").version("1.2.1")
     //id("com.matthewprenger.cursegradle").version("1.4.0")
 }
