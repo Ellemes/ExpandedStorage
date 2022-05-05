c38dd3a012f93dc7fb50218041b5810474365b7c
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2022-05-05 17:45:51 +0100

    use new mod plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("ninjaphenix.gradle.mod").version("6.2.2.0")
+    id("ninjaphenix.gradle.mod").version("1.0.0-alpha.1")
     //id("com.modrinth.minotaur").version("1.2.1")
     //id("com.matthewprenger.cursegradle").version("1.4.0")
 }
