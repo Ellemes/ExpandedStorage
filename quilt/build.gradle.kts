c70bc1450797312bf8cb59f919223b98ab2af903
Ellemes <3llemes@gmail.com>
2022-06-04 14:51:10 +0100

    Append commit to changelog and update mod gradle plugin.

--- quilt/build.gradle.kts
+++ quilt/build.gradle.kts
@@ -73,14 +73,50 @@ dependencies {
 val releaseModTask = tasks.getByName("releaseMod")
 val modVersion = properties["mod_version"] as String
 val modReleaseType = if ("alpha" in modVersion) "alpha" else if ("beta" in modVersion) "beta" else "release"
-val modChangelog = rootDir.resolve("changelog.md").readText(Charsets.UTF_8)
+var modChangelog = rootDir.resolve("changelog.md").readText(Charsets.UTF_8)
 val modTargetVersions = mutableListOf(properties["minecraft_version"] as String)
 val modUploadDebug = System.getProperty("MOD_UPLOAD_DEBUG", "false") == "true" // -DMOD_UPLOAD_DEBUG=true
+
+fun String.execute() = org.codehaus.groovy.runtime.ProcessGroovyMethods.execute(this)
+val Process.text: String? get() = org.codehaus.groovy.runtime.ProcessGroovyMethods.getText(this)
+val commit = "git rev-parse HEAD".execute().text
+modChangelog += "\nCommit: https://github.com/Ellemes/ExpandedStorage/commit/$commit"
 
 (properties["extra_game_versions"] as String).split(",").forEach {
     if (it != "") {
         modTargetVersions.add(it)
     }
+}
+
+curseforge {
+    options(closureOf<me.hypherionmc.cursegradle.Options> {
+        debug = modUploadDebug
+        javaVersionAutoDetect = false
+        javaIntegration = false
+        forgeGradleIntegration = false
+        fabricIntegration = false
+        detectFabricApi = false
+    })
+
+    project(closureOf<me.hypherionmc.cursegradle.CurseProject> {
+        apiKey = System.getenv("CURSEFORGE_TOKEN")
+        id = properties["curseforge_project_id"]
+        releaseType = modReleaseType
+        mainArtifact(tasks.getByName("minJar"), closureOf<me.hypherionmc.cursegradle.CurseArtifact> {
+            displayName = project.name.capitalized() + " " + modVersion
+            artifact = tasks.getByName("minJar")
+        })
+        relations(closureOf<me.hypherionmc.cursegradle.CurseRelation> {
+            //requiredDependency("qsl")
+            requiredDependency("ellemes-container-library")
+            optionalDependency("htm")
+            optionalDependency("carrier")
+            optionalDependency("towelette")
+        })
+        changelogType = "markdown"
+        changelog = modChangelog
+        gameVersionStrings = listOf(project.name.capitalized(), "Java " + java.targetCompatibility.majorVersion) + mutableListOf("1.19-Snapshot") // todo: remove hack
+    })
 }
 
 modrinth {
@@ -105,5 +141,5 @@ modrinth {
 }
 
 afterEvaluate {
-    releaseModTask.dependsOn(tasks.getByName("modrinth"))
+    releaseModTask.finalizedBy(listOf("modrinth", "curseforge" + properties["curseforge_project_id"]))
 }
